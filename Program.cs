c6c3475a9a9b6aa61f12106c9471aebd14e5b895
AyaZabalawi <128138041+AyaZabalawi@users.noreply.github.com>
2024-07-12 15:56:39 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using ServiceContracts;
+using Services;
+using StocksApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
